bc76040bef7dc3e70244efbf71a79ca3e7abdad7
Nu4ego <144213766+Nu4ego@users.noreply.github.com>
2024-01-26 05:59:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,203 @@
+// let a = 10; // 1 задача
+
+// if (a === 10) {
+// console.log('Верно');
+// } else {
+// console.log('Неверно');
+// }
+
+// if (min >= 0 && min <= 14) {     // 2 задача
+//     console.log("Первая четверть");
+// } else if (min >= 15 && min <= 29) {
+//     console.log("Вторая четверть");
+// } else if (min >= 30 && min <= 44) {
+//     console.log("Третья четверть");
+// } else {
+//     console.log("Четвертая четверть");
+// }
+
+// let lang = 'ru'; // 3 задача
+// let arr;
+
+// if (lang === 'ru') {
+//   arr = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+// } else if (lang === 'en') {
+//   arr = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+// }
+
+// let lang = 'ru';
+// let arr;
+
+// switch (lang) {
+//   case 'ru':
+//     arr = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+//     break;
+//   case 'en':
+//     arr = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+//     break;
+// }
+
+// let lang = 'ru';
+// let arr = {
+//   'ru': ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
+//   'en': ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+// }[lang];
+
+
+// var a = 1; // работа с if-else
+// if (a === 0) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+// if (a > 0) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+// if (a < 0) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+// if (a >= 0) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+// if (a <= 0) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+// if (a !== 0) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+// if (a === 'test') {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+// if (a === '1') {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+// var a = 5; // Работа с && (и) и || (или)
+// if (a > 0 && a < 5) {
+// console.log('Верно');
+// } else {
+// console.log('Неверно');
+// }
+
+// var a = 5;
+// if (a === 0 || a === 2) {
+// a += 7;
+// } else {
+// a /= 10;
+// }
+// console.log(a);
+
+// var a = 1;
+// var b = 3;
+// if (a <= 1 && b >= 3) {
+// console.log(a + b);
+// } else {
+// console.log(a - b);
+// }
+
+// var a = 3;
+// var b = 5;
+// if ((a > 2 && a < 11) || (b >= 6 && b < 14)) {
+// console.log('Верно');
+// } else {
+// console.log('Неверно');
+// }
+
+
+// let num = 3; // Задачи на switch-case
+// let result;
+// switch (num) {
+// case 1:
+// result = 'зима';
+// break;
+// case 2:
+// result = 'весна';
+// break;
+// case 3:
+// result = 'лето';
+// break;
+// case 4:
+// result = 'осень';
+// break;
+// }
+// console.log(result); 
+////////////////////////////
+// let day = 16;
+// let decade;
+// switch (Math.ceil(day / 10)) {
+// case 1:
+// decade = 'первая';
+// break;
+// case 2:
+// decade = 'вторая';
+// break;
+// case 3:
+// decade = 'третья';
+// break;
+// }
+// console.log(decade);
+////////////////////////////
+// let month = 8;
+// let season;
+
+// switch (month) {
+// case 1:
+// case 2:
+// case 12:
+// season = 'зима';
+// break;
+// case 3:
+// case 4:
+// case 5:
+// season = 'весна';
+// break;
+// case 6:
+// case 7:
+// case 8:
+// season = 'лето';
+// break;
+// case 9:
+// case 10:
+// case 11:
+// season = 'осень';
+// break;
+// }
+// console.log(season);
+
+// let str1 = 'abcde';
+// if (str1.charAt(0) === 'a') console.log('да'); else console.log('нет');
+////////////////////////////////////
+// let str2 = '12345';
+// if (['1', '2', '3'].includes(str2.charAt(0))) console.log('да'); else console.log('нет');
+/////////////////////////////////
+// let str3 = '123';
+// let sum = parseInt(str3.charAt(0)) + parseInt(str3.charAt(1)) + parseInt(str3.charAt(2));
+// console.log(sum);
+////////////////////////////////////////
+// let str4 = '123456';
+// let firstThreeSum = parseInt(str4.charAt(0)) + parseInt(str4.charAt(1)) + parseInt(str4.charAt(2));
+// let secondThreeSum = parseInt(str4.charAt(3)) + parseInt(str4.charAt(4)) + parseInt(str4.charAt(5));
+// if (firstThreeSum === secondThreeSum) console.log('да');
+// else console.log('нет');
+
